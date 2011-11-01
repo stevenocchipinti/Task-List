fe54f9e342abf85a2432e007ea405a8dd131b1b2
Steven Occhipinti <dev@stevenocchipinti.com>
2011-11-01 21:42:16 +1100

    Show progress text next to the image
    This replaces the alt and title text

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,6 +2,6 @@ module TasksHelper
   def progress_image(progress)
     css = "progress progress-#{(progress*24).floor}"
     text = "#{(progress * 100).floor}%"
-    content_tag("span", "", :class => css, :alt => text, :title => text)
+    content_tag("span", text, :class => css)
   end
 end
