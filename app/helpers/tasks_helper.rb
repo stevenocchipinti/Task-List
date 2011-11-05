3151c6d39a787a04366f13182e78ebfae0a39e20
Steven Occhipinti <dev@stevenocchipinti.com>
2011-11-06 01:12:50 +1100

    Implement nested tasks in the views
    This is achieved with the help of the RailsCast on Ancestry:
    http://railscasts.com/episodes/262-trees-with-ancestry

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,7 +1,16 @@
 module TasksHelper
+
   def progress_image(progress)
     css = "progress progress-#{(progress*24).floor}"
     text = "#{(progress * 100).floor}%"
     content_tag("div", text, :class => css)
   end
+
+
+  def nested_tasks(tasks)
+    tasks.map do |task, sub_tasks|
+      render(task) + content_tag(:div, nested_tasks(sub_tasks), :class => "nested_tasks")
+    end.join.html_safe
+  end
+
 end
