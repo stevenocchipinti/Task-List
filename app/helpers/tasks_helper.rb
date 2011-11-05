2143631cbe3b9e0d051f79d751a823d4efe67d3b
Steven Occhipinti <dev@stevenocchipinti.com>
2011-11-05 14:13:28 +1100

    Add some more basic CSS stuff
    The .body can be hidden to compact the list nicely

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,6 +2,6 @@ module TasksHelper
   def progress_image(progress)
     css = "progress progress-#{(progress*24).floor}"
     text = "#{(progress * 100).floor}%"
-    content_tag("span", text, :class => css)
+    content_tag("div", text, :class => css)
   end
 end
