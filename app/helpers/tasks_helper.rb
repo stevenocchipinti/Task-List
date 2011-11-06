5f5f42a119642b6ce76769f23fa109dad8f61903
Steven Occhipinti <dev@stevenocchipinti.com>
2011-11-06 23:37:24 +1100

    Implement jquery.rating.js

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -8,12 +8,21 @@ module TasksHelper
 
 
   def nested_tasks(tasks)
-    tasks.map do |task, sub_tasks|
+    tasks.collect do |task, sub_tasks|
       render(task, :subtasks => sub_tasks.count) + content_tag(
         :div,
         nested_tasks(sub_tasks),
         :class => "nested_tasks"
       )
+    end.join.html_safe
+  end
+
+
+  def rating_tags(name, container, selected = nil, options = {})
+    container.collect do |element|
+      text, value = option_text_and_value(element).collect { |item| item.to_s }
+      opts = {:class => :star, :title => text}.merge(options)
+      radio_button_tag(name, value, (selected.to_s == value), opts)
     end.join.html_safe
   end
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -6,29 +6,46 @@ class Task < ActiveRecord::Base
   validates :completion,
     :allow_nil => true, # nil means auto-calculate
     :numericality => {
-      :greater_then_or_equal_to => 0,
+      :greater_than_or_equal_to => 0,
       :less_than_or_equal_to => 1
       }
   validates :estimate,
     :allow_nil => true, # nil means auto-calculate
     :numericality => {
       :only_integer => true,
-      :greater_then_or_equal_to => 0,
-      :less_than_or_equal_to => 5
+      :greater_than => 0,
+      :less_than_or_equal_to => 3
      }
   validates :importance,
     :allow_nil => true, # nil means auto-calculate
     :numericality => {
       :only_integer => true,
-      :greater_then_or_equal_to => 0,
-      :less_than_or_equal_to => 5
+      :greater_than => 0,
+      :less_than_or_equal_to => 3
     }
   validates :urgency,
     :allow_nil => true, # nil means auto-calculate
     :numericality => {
       :only_integer => true,
-      :greater_then_or_equal_to => 0,
-      :less_than_or_equal_to => 5
+      :greater_than => 0,
+      :less_than_or_equal_to => 3
     }
+
+
+  def self.completion_options
+    (1..10).collect{|x| ["#{x*10}%", x/10.0]}
+  end
+
+  def self.estimate_options
+    [["Not long", 1], ["Average", 2], ["Long time", 3]]
+  end
+
+  def self.importance_options
+    [["Not important", 1], ["Important", 2], ["Very important", 3]]
+  end
+
+  def self.urgency_options
+    [["Not urgent", 1], ["Urgent", 2], ["Very urgent", 3]]
+  end
 
 end
