7f55944bf547d7a7b3c869971556f021977d5e65
Steven Occhipinti <dev@stevenocchipinti.com>
2011-11-06 17:56:25 +1100

    Add validations to the task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,34 @@
 class Task < ActiveRecord::Base
+
   has_ancestry
+
+  validates :name, :presence => true
+  validates :completion,
+    :allow_nil => true, # nil means auto-calculate
+    :numericality => {
+      :greater_then_or_equal_to => 0,
+      :less_than_or_equal_to => 1
+      }
+  validates :estimate,
+    :allow_nil => true, # nil means auto-calculate
+    :numericality => {
+      :only_integer => true,
+      :greater_then_or_equal_to => 0,
+      :less_than_or_equal_to => 5
+     }
+  validates :importance,
+    :allow_nil => true, # nil means auto-calculate
+    :numericality => {
+      :only_integer => true,
+      :greater_then_or_equal_to => 0,
+      :less_than_or_equal_to => 5
+    }
+  validates :urgency,
+    :allow_nil => true, # nil means auto-calculate
+    :numericality => {
+      :only_integer => true,
+      :greater_then_or_equal_to => 0,
+      :less_than_or_equal_to => 5
+    }
+
 end
